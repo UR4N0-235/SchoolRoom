63c45b81049e1d1713cba57828f409773e5ff415
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-09-18 10:12:11 -0300

    first commit

---
+++ src/main/java/com/ur4n0/schoolroom/SchoolRoomApplication.java
@@ -0,0 +1,13 @@
+package com.ur4n0.schoolroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolRoomApplication.class, args);
+	}
+
+}
