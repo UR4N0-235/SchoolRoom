4ac12b9b9dc4915601fa40e4eb940048b25da2a6
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-09-22 11:56:49 -0300

    build: database seeders

---
+++ src/main/resources/sql/1-teacher.sql
@@ -0,0 +1,11 @@
+INSERT INTO teacher_model (name, rm, password)
+VALUES ('Maria da Silva', '5678', 'senha123'),
+    ('José Souza', '9876', 'segura456'),
+    ('Ana Pereira', '5432', 'minhasenha'),
+    ('Carlos Santos', '6543', 'outrasenha'),
+    ('Sandra Oliveira', '4321', 'senhasecreta'),
+    ('Pedro Lima', '8765', 'senhatop'),
+    ('Isabela Gomes', '8761', 'senhagrande'),
+    ('Fernando Alves', '7824', 'senha1234'),
+    ('Lúcia Martins', '1290', 'novasenha'),
+    ('Ricardo Ferreira', '9087', 'supersegura');
---
+++ src/main/resources/sql/2-room.sql
@@ -0,0 +1,14 @@
+INSERT INTO room_model (number, type)
+VALUES (1, 'SALA_DE_AULA'),
+    (2, 'SALA_DE_AULA'),
+    (3, 'SALA_DE_AULA'),
+    (4, 'SALA_DE_AULA'),
+    (5, 'SALA_DE_AULA'),
+    (6, 'SALA_DE_AULA'),
+    (7, 'SALA_DE_AULA'),
+    (8, 'SALA_DE_AULA'),
+    (9, 'SALA_DE_AULA'),
+    (10, 'SALA_DE_AULA'),
+    (11, 'SALA_DE_AULA'),
+    (12, 'SALA_DE_AULA'),
+    (13, 'SALA_DE_AULA');
