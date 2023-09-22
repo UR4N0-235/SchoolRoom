348dc593aa0238731903c2330702af71ec8683ca
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-09-22 11:53:43 -0300

    feat: basic schedule operations

--- src/main/java/com/ur4n0/schoolroom/schedule/ScheduleRepository.java
+++ src/main/java/com/ur4n0/schoolroom/schedule/ScheduleRepository.java
@@ -1,6 +1,8 @@
 package com.ur4n0.schoolroom.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,6 +14,13 @@ import com.ur4n0.schoolroom.room.RoomModel;
 public interface ScheduleRepository extends JpaRepository<ScheduleModel, Long>{
     List<ScheduleModel> findByTeacher(TeacherModel teacher);
     List<ScheduleModel> findByRoom(RoomModel room);
-    List<ScheduleModel> findByDateBetween(LocalDateTime startOf, LocalDateTime endOf);
-
+    List<ScheduleModel> findByStartDateBetween(LocalDateTime startOf, LocalDateTime endOf);
+    List<ScheduleModel> findByStartDateBetweenAndEndDateBetween(LocalDateTime start, LocalDateTime end, LocalDateTime start2, LocalDateTime end2);
+    List<ScheduleModel> findByTeacherAndRoom(TeacherModel teacher, RoomModel room);
+    @Query("SELECT s FROM ScheduleModel s WHERE s.room.id = :roomId " +
+            "AND (:startDate BETWEEN s.startDate AND s.endDate " +
+            "OR :endDate BETWEEN s.startDate AND s.endDate)")
+    List<ScheduleModel> findConflictingSchedules(@Param("roomId") Long roomId,
+                                                 @Param("startDate") LocalDateTime startDate,
+                                                 @Param("endDate") LocalDateTime endDate);
 }
--- src/main/java/com/ur4n0/schoolroom/schedule/ScheduleService.java
+++ src/main/java/com/ur4n0/schoolroom/schedule/ScheduleService.java
@@ -17,8 +17,20 @@ public class ScheduleService {
     @Autowired
     private ScheduleRepository scheduleRepository;
 
+    public boolean isUnoccupiedRoom(ScheduleModel schedule) {
+        return scheduleRepository
+                .findConflictingSchedules(
+                        schedule.getRoom().getId(),
+                        schedule.getStartDate(),
+                        schedule.getEndDate())
+                .isEmpty();
+    }
+
     public ScheduleModel createSchedule(ScheduleModel schedule) {
-        return scheduleRepository.save(schedule);
+        if(isUnoccupiedRoom(schedule))
+            return scheduleRepository.save(schedule);
+        else
+            throw new IllegalArgumentException("room not empty in this datetime");
     }
 
     public Optional<ScheduleModel> getScheduleById(Long id) {
@@ -28,17 +40,17 @@ public class ScheduleService {
     public ScheduleModel updateSchedule(Long id, ScheduleModel updatedSchedule) {
         Optional<ScheduleModel> existingSchedule = scheduleRepository.findById(id);
 
-        if (existingSchedule.isPresent()) {
-            ScheduleModel scheduleToUpdate = existingSchedule.get();
-            scheduleToUpdate.setDate(updatedSchedule.getDate());
-
-            scheduleToUpdate.setTeacher(updatedSchedule.getTeacher());
-            scheduleToUpdate.setRoom(updatedSchedule.getRoom());
-
-            return scheduleRepository.save(scheduleToUpdate);
-        } else {
+        if (!existingSchedule.isPresent())
             throw new IllegalArgumentException("Schedule with ID " + id + " not found.");
-        }
+
+        ScheduleModel scheduleToUpdate = existingSchedule.get();
+        scheduleToUpdate.setStartDate(updatedSchedule.getStartDate());
+        scheduleToUpdate.setEndDate(updatedSchedule.getEndDate());
+
+        scheduleToUpdate.setTeacher(updatedSchedule.getTeacher());
+        scheduleToUpdate.setRoom(updatedSchedule.getRoom());
+
+        return scheduleRepository.save(scheduleToUpdate);
     }
 
     public void deleteSchedule(Long id) {
@@ -57,20 +69,24 @@ public class ScheduleService {
         return scheduleRepository.findByRoom(room);
     }
 
+    public List<ScheduleModel> getSchedulesByTeacherAndRoom(TeacherModel teacher, RoomModel room) {
+        return scheduleRepository.findByTeacherAndRoom(teacher, room);
+    }
+
     public List<ScheduleModel> getSchedulesByDay(LocalDate date) {
         LocalDateTime startOfDay = date.atStartOfDay();
         LocalDateTime endOfDay = date.atTime(23, 59, 59);
-        return scheduleRepository.findByDateBetween(startOfDay, endOfDay);
+        return scheduleRepository.findByStartDateBetween(startOfDay, endOfDay);
     }
-    
+
     public List<ScheduleModel> getSchedulesByDayRange(LocalDate startDateRange, LocalDate endDateRange) {
         LocalDateTime startOfDay = startDateRange.atStartOfDay();
         LocalDateTime endOfDay = endDateRange.atTime(23, 59, 59);
-        return scheduleRepository.findByDateBetween(startOfDay, endOfDay);
+        return scheduleRepository.findByStartDateBetween(startOfDay, endOfDay);
     }
 
-    public List<ScheduleModel> getSchedulesByDateAndTimeRange(LocalDateTime startDateRange, LocalDateTime endDateRange) {
-        return scheduleRepository.findByDateBetween(startDateRange, endDateRange);
+    public List<ScheduleModel> getSchedulesByDateAndTimeRange(LocalDateTime startDateRange,
+            LocalDateTime endDateRange) {
+        return scheduleRepository.findByStartDateBetween(startDateRange, endDateRange);
     }
 }
-
