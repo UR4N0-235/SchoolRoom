ae9f324ffe78e26923f8b7371a07eff65530111a
UR4N0-235 <matheus.fernandes92.235@protonmail.com>
2023-09-22 11:52:06 -0300

    refactor:  added types of room

--- src/main/java/com/ur4n0/schoolroom/room/RoomModel.java
+++ src/main/java/com/ur4n0/schoolroom/room/RoomModel.java
@@ -3,10 +3,13 @@ package com.ur4n0.schoolroom.room;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ur4n0.schoolroom.schedule.ScheduleModel;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,16 +27,27 @@ public class RoomModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "number", unique = true, nullable = false)
+    @Column(name = "type", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private RoomType type;
+
+    @Column(name = "number", nullable = false)
     private int number;
 
     @OneToMany(mappedBy = "room")
+    @JsonIgnore
     private List<ScheduleModel> schedules = new ArrayList<>();
 
     public RoomModel(Long id, int number) {
         this.id = id;
         this.number = number;
+        this.type = RoomType.SALA_DE_AULA; // default
     }
 
-    
+    public RoomModel(Long id, int number, RoomType type) {
+        this.id = id;
+        this.number = number;
+        this.type = type;
+    }
+
 }
---
+++ src/main/java/com/ur4n0/schoolroom/room/RoomType.java
@@ -0,0 +1,7 @@
+package com.ur4n0.schoolroom.room;
+
+public enum RoomType {
+    SALA_DE_AULA,
+    MAKER,
+    INFORMATICA
+}
